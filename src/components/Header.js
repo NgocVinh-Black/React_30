513591f7c893f257b3fe4d0e4137317eaed11918
ngoc_vinh <ngocvinh1923@gmail.com>
2023-11-24 10:41:50 +0700

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import '../index.css';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className="header bg-dark">
+        <div className="container">
+          <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid header_logo">
+              <a className="navbar-brand" href="#">
+                Start Bootstrap
+              </a>
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className="navbar-toggler-icon" />
+              </button>
+              <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav ms-auto mb-2">
+                  <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">
+                      Home
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">
+                      About
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">
+                      Contact
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </div>
+    );
+  }
+}
